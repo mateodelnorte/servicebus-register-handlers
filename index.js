65a51ab824e5059150d98c1ffc2f42385a81bb65
Matt Walters <mattwalters5@gmail.com>
2015-10-16 17:39:00 -0400

    adding ability to subscribe many handlers to same routing keys. adding ability to filter by message properties

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var debug = require('debug')('register-handlers');
 var extend = require('extend');
 var fs = require('fs');
@@ -5,6 +6,7 @@ var objectifyFolder = require('objectify-folder');
 var path = require('path');
 var trace = require('debug')('register-handlers:trace');
 var util = require('util');
+var warn = require('debug')('register-handlers:warn');
 
 function prepareOptions (options) {
   options = options || {};
@@ -18,8 +20,25 @@ function prepareOptions (options) {
   if (options.path) {
     options.handlers = objectifyFolder({
       fn: function (mod, result) {
-        if ( ! mod.routingKey || ! (mod.listen || mod.subscribe)) return;
-        result[(mod.listen ? 'listen.' : 'subscribe.') + mod.routingKey] = mod;
+
+        if ( ! mod.queueName || ! (mod.listen || mod.subscribe)) return;
+
+        var key = (mod.listen ? 'listen.' : 'subscribe.') + (mod.routingKey === undefined ? mod.queueName : mod.routingKey);
+
+        if (mod.where) {
+
+          if ( ! result[key]) {
+            result[key] = [];
+          }
+
+          if (mod.where && typeof mod.where !== 'function') throw new Error('module.exports.where must be of type function and return a boolean statement');
+
+          result[key].push(mod);
+
+        } else {
+          result[key] = mod;
+        }
+
       },
       path: options.path
     });
@@ -37,41 +56,58 @@ module.exports = function (options) {
   Object.keys(options.handlers).forEach(function (key) {
 
     var mod = options.handlers[key];
-    var handler = mod.listen || mod.subscribe;
-    var method = mod.subscribe ? 'subscribe' : 'listen';
-    var routingKey = mod.routingKey;
 
-    debug('%sing to %s', method === 'subscribe' ? method.slice(0, -1) : method, routingKey);
+    if ( ! (mod.routingKey || mod.queueName) && ! (mod instanceof Array)) return;
+
+    var firstOrOnlyMod = ((mod instanceof Array) ? mod[0] : mod);
+
+    var handler =  firstOrOnlyMod.listen || firstOrOnlyMod.subscribe;
+    var method = firstOrOnlyMod.subscribe ? 'subscribe' : 'listen';
+    var routingKey = firstOrOnlyMod.routingKey;
 
     var rk = method === 'subscribe' ?
-      (mod.queueName) ? mod.queueName :
+      (firstOrOnlyMod.queueName) ? firstOrOnlyMod.queueName :
         (options.queuePrefix !== undefined ? util.format(options.queuePrefix + '-.%s', routingKey) : routingKey) :
-          routingKey;
+          routingKey || firstOrOnlyMod.queueName;
+
+    debug('%sing to %s', method === 'subscribe' ? method.slice(0, -1) : method, rk);
 
     function handleError (msg, err) {
       debug('error handling message with cid ', msg.cid);
       debug(err.stack || err.message || err);
-      if (mod.ack) msg.handle.reject(function () {
+      if (firstOrOnlyMod.ack) msg.handle.reject(function () {
         throw err;
       });
     }
 
-    self.bus[method](rk, mod, function (msg, message) {
+    self.bus[method](rk, firstOrOnlyMod, function (msg, message) {
 
-      trace('handling message: %j', msg);
+      var thisModule = mod;
 
       var context = {
+        queueName: message.fields.queueName,
         routingKey: message.fields.routingKey,
         correlationId: message.properties.correlationId
       };
 
-      handler.call(context, msg, function (err) {
+      if (thisModule instanceof Array) {
+        thisModule = thisModule.filter(function (m) {
+          return m.where(msg);
+        });
+      } else {
+        thisModule = [thisModule];
+      }
+
+      if (thisModule.length === 0 && firstOrOnlyMod.ack) msg.handle.reject();
+
+      trace('handling message: %j', msg);
+
+      async.map(thisModule, function (m, cb) {
+        m[method].call(context, msg, cb);
+      }, function (err) {
         if (err) return (options.handleError || handleError).call(context, msg, err);
-
-        if (mod.ack) msg.handle.ack();
-
+        if (firstOrOnlyMod.ack) msg.handle.ack();
         trace('handled message: %j', msg);
-
       });
 
     });
